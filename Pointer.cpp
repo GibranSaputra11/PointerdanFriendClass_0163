f0b539e010767059a19e0b6dcb63fedfe0134880
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-05 15:20:35 +0700

    add simple logic pointer

--- Pointer.cpp
+++ Pointer.cpp
@@ -28,4 +28,18 @@ int main(){
     pds->nama = "Reza";
     cout << "Alamat Memori = " << pds << endl;
     pds->tampilNama();
+
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "Alamat memori a = " << &a << endl;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+    c = &b;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
 }
