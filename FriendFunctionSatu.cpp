396da6a6dd57e9bcd57c84e26d6773d15e660b6d
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-05 16:03:23 +0700

    add clas mahasiswa dan friend function

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -2,6 +2,19 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private:
+    string nama;
+
+    public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b){
+    a.nama = b;
+    cout << a.nama;
+}
+
 int main (){
-    
+
 }
