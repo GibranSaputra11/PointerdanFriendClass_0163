0a02f53a2a2b82106aef138fdb18a2784970ad0e
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-05 16:12:12 +0700

    add class pelajar

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;
+class manusia{
+    public:
+    void showNilaiPelajar(pelajar &x);
+    void showNilaiPelajarDua(pelajar &y);
+};
+
 int main(){
-    
+
 }
